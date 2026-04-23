8bb8c742fdf24b683afc78ad302f3160c6e49aea
Richard PHAN CHAN THE <43934086+richardpct@users.noreply.github.com>
2026-04-23 22:23:38 +0200

    migrate to tofu

---
+++ envs/dev/01-network/outputs.tf
@@ -0,0 +1,43 @@
+output "vpc_id" {
+  value = module.network.vpc_id
+}
+
+output "subnet_public_bastion_id" {
+  value = module.network.subnet_public_bastion_id
+}
+
+output "subnet_private_database_id" {
+  value = module.network.subnet_private_database_id
+}
+
+output "subnet_private_web_id" {
+  value = module.network.subnet_private_web_id
+}
+
+output "sg_bastion_id" {
+  value = module.network.sg_bastion_id
+}
+
+output "sg_database_id" {
+  value = module.network.sg_database_id
+}
+
+output "sg_web_id" {
+  value = module.network.sg_web_id
+}
+
+output "aws_eip_bastion_id" {
+  value = module.network.aws_eip_bastion_id
+}
+
+output "alb_target_group_web_arn" {
+  value = module.network.alb_target_group_web_arn
+}
+
+output "iam_instance_profile_name" {
+  value = module.network.iam_instance_profile_name
+}
+
+output "ssh_key" {
+  value = module.network.ssh_key
+}
---
+++ modules/network/outputs.tf
@@ -0,0 +1,43 @@
+output "vpc_id" {
+  value = aws_vpc.my_vpc.id
+}
+
+output "subnet_public_bastion_id" {
+  value = aws_subnet.public_bastion[*].id
+}
+
+output "subnet_private_database_id" {
+  value = aws_subnet.private_database[*].id
+}
+
+output "subnet_private_web_id" {
+  value = aws_subnet.private_web[*].id
+}
+
+output "sg_bastion_id" {
+  value = aws_security_group.bastion.id
+}
+
+output "sg_database_id" {
+  value = aws_security_group.database.id
+}
+
+output "sg_web_id" {
+  value = aws_security_group.web.id
+}
+
+output "aws_eip_bastion_id" {
+  value = aws_eip.bastion.id
+}
+
+output "alb_target_group_web_arn" {
+  value = aws_lb_target_group.web.arn
+}
+
+output "iam_instance_profile_name" {
+  value = aws_iam_instance_profile.profile.name
+}
+
+output "ssh_key" {
+  value = aws_key_pair.deployer.key_name
+}
